e188eaad85194f2520aa56134f5e0705bb27bcbf
Ruhul Amin <ruhulaminbabu93@gmail.com>
2024-11-01 20:14:57 +0600

    46-3 Explore Ref and controlled and uncontrolled components

--- src/__form__.js
+++ src/__form__.js
@@ -1,4 +1,10 @@
 /**
  * 1. use form tag and onSubmit handler with event (e)
  * form e access e.target.[name of the input feield].value
+ * 
+ * 2. controlled element: use individual feild stae for each input field
+ * 
+ * 3. controlled element: one object holding all thw input fields value
+ * 
+ * 4. Uncontroled element: useRef to create a reference to the element and access value by using like: nameRef.current.value
  */
